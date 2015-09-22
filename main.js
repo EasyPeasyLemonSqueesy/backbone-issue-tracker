00a13876f915b0f2a5c46de081039014aa126443
Joe Fraley <jose.fraley@gmail.com>
2015-09-22 13:23:41 -0700

    includes major outline of TaskView

--- main.js
+++ main.js
@@ -7,11 +7,25 @@ $(function() { //when DOM is ready...
 		{username:'Person3'}
 	]);
 
-	app.tasks = new TaskCollection([
-		// test data here
+	app.tasks = new IssueCollection([
+		{
+			title: 'Test the issue render',
+			description: 'make sure that the page can render tasks',
+			creator: 'Joseph'
+		},
+		{
+			title: 'Test the issue Collection',
+			description: 'make sure the collection holds more than one task',
+			creator: 'Adam'
+		},
+		{
+			title: 'Test the button',
+			description: 'make sure each task has a dropdown of statuses',
+			creator: 'Nate'
+		}
 	]);
 
 	app.gui = new GUI(app.users,
 						app.tasks,
 						'#app');// selector of main div
-}
+});
