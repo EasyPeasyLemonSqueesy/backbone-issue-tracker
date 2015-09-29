ac4d5281d629db6fb15b55556f508fa17af56124
Nathaniel Bennett <nathaniel.w.bennett@gmail.com>
2015-09-29 11:50:02 -0700

    updated a post path for use with taks id

--- reset.js
+++ reset.js
@@ -7,15 +7,17 @@ var db = require('orchestrate')('e12dde45-d9d1-40dc-b7cc-185b1b716f7f');
 console.log("I'm reseting");
 
 db.deleteCollection('users');
-db.deleteCollection('task');
+db.deleteCollection('tasks');
 
-db.post('task', {
+db.post('tasks', {
   "title" : "1st task",
   "description" : "Dummy data task",
   "creator" : "Nathaniel",
   "id" : "Mon Sep 28 2015 20:01:04 GMT-0700 (Pacific Daylight Time)",
   "assignee" : "",
   "status" : "unassigned"
+}).fail(function(mess){
+  console.log(mess);
 });
 
 db.post('users', {
